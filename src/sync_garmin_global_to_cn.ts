5cbc9743904995b4ea9de0808e42fd0bf71d2db2
Mathew P. Jones <xiangzhuyuan@gmail.com>
2024-11-08 17:28:09 +0900

    Update sync_garmin_global_to_cn.ts

--- src/sync_garmin_global_to_cn.ts
+++ src/sync_garmin_global_to_cn.ts
@@ -7,6 +7,7 @@ const BARK_KEY = process.env.BARK_KEY ?? BARK_KEY_DEFAULT;
 
 try {
     syncGarminGlobal2GarminCN();
+    axios.get(`https://api.day.app/${BARK_KEY}/Garmin Global->CN 同步数据done`);
 } catch (e) {
     axios.get(
         `https://api.day.app/${BARK_KEY}/Garmin CN -> Garmin Global 同步数据运行失败了，快去检查！/${e.message}`);
