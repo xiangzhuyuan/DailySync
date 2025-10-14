3e3569ba4fcb13e3dc5be7370a30230bca968c18
Mathew P. Jones <xiangzhuyuan@gmail.com>
2025-10-14 16:23:34 +0900

    Update check_book.rb
    upper case

--- check_book.rb
+++ check_book.rb
@@ -2,7 +2,7 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-uri                           = URI.parse(ENV['target'])
+uri                           = URI.parse(ENV['TARGET'])
 request                       = Net::HTTP::Get.new(uri)
 request["Accept"]             = "application/json, text/javascript, */*; q=0.01"
 request["Accept-Language"]    = "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6"
@@ -18,7 +18,7 @@ request["X-Requested-With"]   = "XMLHttpRequest"
 request["Sec-Ch-Ua"]          = "\"Google Chrome\";v=\"141\", \"Not?A_Brand\";v=\"8\", \"Chromium\";v=\"141\""
 request["Sec-Ch-Ua-Mobile"]   = "?0"
 request["Sec-Ch-Ua-Platform"] = "\"macOS\""
-request['Cookie']             = ENV['Cookie']
+request['Cookie']             = ENV['COOKIE']
 
 req_options = {
   use_ssl: uri.scheme == "https",
@@ -33,7 +33,7 @@ yes = JSON.parse(response.body)['akiList'].length
 puts "#{Time.now.to_s} check with aki:#{yes} #{response.code}"
 if yes != 0
 
-  uri                        = URI.parse(ENV['endpoint'])
+  uri                        = URI.parse(ENV['ENDPOINT'])
   request                    = Net::HTTP::Get.new(uri)
   request["Accept"]          = "application/json, text/javascript, */*; q=0.01"
   request["Accept-Language"] = "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6"
